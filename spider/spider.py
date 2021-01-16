bd3d733b672af063b104e3ddb73c51e19f0b2184
xcdq <xcdqaaa@qq.com>
2021-01-16 15:20:04 +0800

    for spider

---
+++ spider/spider.py
@@ -0,0 +1,33 @@
+from urllib import request
+import re
+
+
+class Spider():
+
+    url = 'http://txt.soushupan.xyz:18866/forum.php'
+    root_pattern = '<ul class="category_newlist">([\s\S]*?)</ul>'
+    name_pattern = '<strong>([\s\S]*?)</strong>'
+
+    def __fetch_content(self):
+        r = request.urlopen(Spider.url)
+        htmls = r.read()
+        # htmls = str(htmls, encoding='GDK')
+        # print(htmls.decode('gbk'))
+        # root_html = re.findall(Spider.root_pattern, htmls.decode('gbk'))
+        # print(">>>>")
+        # print(root_html)
+        return htmls
+
+    def __analysis(self, htmls):
+        root_html = re.findall(Spider.root_pattern, htmls.decode('gbk'))
+        name_html = re.findall(Spider.name_pattern, htmls.decode('gbk'))
+
+        a = 1
+
+    def go(self):
+        htmls = self.__fetch_content()
+        self.__analysis(htmls)
+
+
+spider = Spider()
+spider.go()
