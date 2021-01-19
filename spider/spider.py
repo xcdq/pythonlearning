c424d711b384c5728cf3aeac053bac79ae85db85
xcdq <xcdqaaa@qq.com>
2021-01-19 19:18:33 +0800

    删除git0102

--- spider/spider.py
+++ spider/spider.py
@@ -7,6 +7,7 @@ class Spider():
     url = 'http://txt.soushupan.xyz:18866/forum.php'
     root_pattern = '<ul class="category_newlist">([\s\S]*?)</ul>'
     name_pattern = '标题: <strong>([\s\S]*?)</strong>'
+    box_pattern = '<div class=".*?box">[\s\S]*?</div>'
 
     def __fetch_content(self):
         r = request.urlopen(Spider.url)
@@ -19,10 +20,19 @@ class Spider():
         return htmls
 
     def __analysis(self, htmls):
-        root_html = re.findall(Spider.root_pattern, htmls.decode('gbk'))
-        name_html = re.findall(Spider.name_pattern, htmls.decode('gbk'))
-        for x in name_html:
-            print(x)
+        # print(htmls.decode('gbk'))
+        with open('./text/shu.html', 'w', encoding='gbk') as fp:
+            fp.write(htmls.decode('gbk'))
+        box_html = re.findall(Spider.box_pattern, htmls.decode('gbk'))
+        # root_html = re.findall(Spider.root_pattern, htmls.decode('gbk'))
+        for x in box_html:
+            name_box = re.findall('</span>(.*?)</h4>', x)
+            name_html = re.findall(Spider.name_pattern, x)
+            print(name_box[0])
+            print()
+            for y in name_html:
+                print(y)
+            print('>>>>>>>>>>>>>>\n\n\n\n')
         a = 1
 
     def go(self):
