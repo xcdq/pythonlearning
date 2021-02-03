07079820a2e22d7b495bb68acf258418e8758950
xcdq <xcdqaaa@qq.com>
2021-02-03 18:20:13 +0800

    可选图片质量&gl爬不能

--- spider/zhanku.py
+++ spider/zhanku.py
@@ -5,19 +5,29 @@ import re
 # url = 'https://www.zcool.com.cn/work/ZNTAzNzY5ODQ=.html'
 # url = 'https://www.zcool.com.cn/work/ZNDg3MTY0MDA=.html'
 # url = 'https://www.zcool.com.cn/work/ZMzIxMjM0NDQ=.html'
-url = input()
-src = re.search('work/(.*)=\.html', url)[1]
+url = input('请输入链接：')
+dx = input('图片质量（低：1、高：2）：')
+# src = re.search('work/(.*)=\.html', url)[1]
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+}
 
 
-def make_dir(src):
+def make_dir(page_text, dx):
     """文件夹创建"""
+    src = re.search('h2>(.*?)<span', page_text, re.S)[1].strip()
+    src = src.replace('|', '')
+    src += ('（高）')if dx == '2' else ('（低）')
+    print(src)
     if not os.path.exists('./text/MN/'+src):
         os.makedirs('./text/MN/'+src)
+    return src
 
 
-def make_save(src, pic, data):
+def make_save(src, pic):
     """保存图片"""
-    img_name = re.search('community/(.*?)@', pic)[1]
+    img_data = requests.get(url=pic, headers=headers).content
+    img_name = re.search('community/(.*?.jpg)', pic)[1]
     # print(img_name)
     img_path = './text/MN/'+src+"/"+img_name
     with open(img_path, 'wb')as fp:
@@ -25,20 +35,16 @@ def make_save(src, pic, data):
         print(img_name, '下载成功！！！')
 
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
-}
 page_text = requests.get(url=url, headers=headers).text
 tree = etree.HTML(page_text)
 pic_list = tree.xpath(
     '//*[@id="body"]/main//div[@class="photo-information-content"]/img/@src')
-# print(page_text)
-# print(page_text.type)
-src = re.search('h2>(.*?)<span', page_text, re.S)[1].strip()
-print(src)
-make_dir(src)
+src = make_dir(page_text, dx)
 for pic in pic_list:
-    img_data = requests.get(url=pic, headers=headers).content
-    make_save(src, pic, img_data)
+    if(dx == '2'):
+        pic = re.search('(.*?\.jpg)', pic)[1]
+    print(pic)
+
+    make_save(src, pic)
     # print(pic)
 print('下载完成')
--- todo/forre.py
+++ todo/forre.py
@@ -2,6 +2,10 @@ import re
 # txt = 'https://img.zcool.cn/community/019d71600966ed11013e399161bbc5.jpg@1280w_1l_0o_100sh.jpg'
 # # all = re.search('community/(.*?)@', txt)[1]
 # print(all)
-txt = " <h2>     遇萤 <span"
-all = re.search('h2>(.*)<span', txt)[1].strip()
-print('a', all, 'a')
+# txt = " <h2>     遇萤 <span"
+# all = re.search('h2>(.*)<span', txt)[1].strip()
+# print('a', all, 'a')
+url = 'https://img.zcool.cn/community/0137735fb3c7b311013ee04dc09f8f.jpg'
+# pic = re.search('(.*?\.jpg)', url)[1]
+pic = re.search('community/(.*)', url)[1]
+print(pic)
